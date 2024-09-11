973de4c4f7a89d22db8311fd04f7fe6b515e2a29
Peter Akor <Akor.peter101@gmail.com>
2024-09-11 14:47:33 +0000

    testing join

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
- SELECT
-      products_id,
-      date_date,
-      orders_id,
-      revenue,
-      quantity,
-      purchase_price,
-      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-  FROM {{ref("stg_raw__sales")}} s
-  LEFT JOIN {{ref("stg_raw__product")}} p
-      USING (products_id)
+SELECT
+    s.products_id,
+    s.date_date,
+    s.orders_id,
+    s.revenue,
+    p.quantity,
+    p.purchase_price,
+    ROUND(p.quantity * p.purchase_price, 2) AS purchase_cost,
+    ROUND(s.revenue - (p.quantity * p.purchase_price), 2) AS margin
+FROM {{ ref("stg_raw__sales") }} s
+LEFT JOIN {{ ref("stg_raw__product") }} p
+    ON s.products_id = p.products_id
